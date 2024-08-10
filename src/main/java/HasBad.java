1802918350a9206068493ed483f1e306a65dcbb3
Zhanybek2 <zhanybekTalip2@gmail.com>
2024-08-10 21:56:26 +0600

    String1/hasbad

---
+++ src/main/java/HasBad.java
@@ -0,0 +1,18 @@
+public class HasBad {
+    public boolean hasBad(String str) {
+        if(str.length() < 3){
+            return false;
+
+            }
+        if ( str.substring(0,3).equals("bad") ||
+                (str.length() >3 && str.substring(1,4).equals("bad"))){
+            return true;
+
+        }
+        return false;
+
+    }
+
+}
+
+
