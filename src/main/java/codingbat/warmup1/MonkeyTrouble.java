6c501a7044983ca4f5c524ebc97301ce4b3bf897
Zhanybek2 <zhanybekTalip2@gmail.com>
2024-08-10 12:23:51 +0600

    Warmup1/monkeyTrouble

---
+++ src/main/java/codingbat/warmup1/MonkeyTrouble.java
@@ -0,0 +1,15 @@
+package codingbat.warmup1;
+
+public class MonkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+
+        if (aSmile && bSmile) {
+            return true;
+        } else if (!aSmile && !bSmile) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
