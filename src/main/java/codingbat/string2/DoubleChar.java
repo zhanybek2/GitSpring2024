96c46242ad6b0a9722191e1c853c164e82af9559
Zhanybek2 <zhanybekTalip2@gmail.com>
2024-08-10 12:02:35 +0600

    String2/DoubleChar

---
+++ src/main/java/codingbat/string2/DoubleChar.java
@@ -0,0 +1,13 @@
+package codingbat.string2;
+
+public class DoubleChar {
+    public String doubleChar(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            result.append(c).append(c);
+        }
+        return result.toString();
+
+    }
+}
