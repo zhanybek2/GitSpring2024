d68ea81fce0d0a494856ac16e027a766d114a803
Zhanybek2 <zhanybekTalip2@gmail.com>
2024-08-10 12:30:19 +0600

    Warmup2/Frontimes

---
+++ src/main/java/codingbat/warmup2/FrontTimes.java
@@ -0,0 +1,29 @@
+package codingbat.warmup2;
+
+public class FrontTimes {
+    public String frontTimes(String str, int n) {
+    int len = str.length();
+
+    String temp = "";
+
+  if (len < 4) {
+
+        for (int i = 0; i < n; i++) {
+
+            temp += str;
+
+        }
+
+    } else {
+
+        for (int j = 0; j < n; j++) {
+            temp += str.substring(0,3);
+
+        }
+
+    }
+
+  return temp;
+
+}
+}
