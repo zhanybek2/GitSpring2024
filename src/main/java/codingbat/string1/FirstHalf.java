04330610fd8967410cc21280ffba51cfe43324d8
Zhanybek2 <zhanybekTalip2@gmail.com>
2024-08-10 13:30:45 +0600

    String1/firstHalf

---
+++ src/main/java/codingbat/string1/FirstHalf.java
@@ -0,0 +1,11 @@
+package codingbat.string1;
+
+public class FirstHalf {
+    public String firstHalf(String str) {
+        int mid = str.length() /2;
+        return str.substring(0,mid);
+
+
+        }
+    }
+}
