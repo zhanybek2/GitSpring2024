558b4f9b562a53612957f23c3536ae76ba56989e
Zhanybek2 <zhanybekTalip2@gmail.com>
2024-08-10 17:11:41 +0600

    String1/endsly

---
+++ src/main/java/codingbat/string1/EndsLy.java
@@ -0,0 +1,13 @@
+package codingbat.string1;
+
+public class EndsLy {
+    public boolean endsLy(String str) {
+        if (str.endsWith("ly")){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+}
