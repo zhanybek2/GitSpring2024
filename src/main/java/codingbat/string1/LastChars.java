6f8428ffeacae17995641200b4a832ae2639b7d2
Zhanybek2 <zhanybekTalip2@gmail.com>
2024-08-10 23:53:41 +0600

    String1/lastChars

---
+++ src/main/java/codingbat/string1/LastChars.java
@@ -0,0 +1,13 @@
+package codingbat.string1;
+
+public class LastChars {
+    public String lastChars(String a, String b) {
+        if (a.length() == 0)
+            a = "@";
+
+        if (b.length() == 0)
+            b = "@";
+
+        return a.substring(0, 1) + b.substring(b.length() - 1, b.length());
+    }
+}
