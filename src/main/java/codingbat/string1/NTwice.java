74eb19b1c29bbc07478a6af9baae6e2d476b101b
Zhanybek2 <zhanybekTalip2@gmail.com>
2024-08-10 17:42:57 +0600

    String1/nTwice

---
+++ src/main/java/codingbat/string1/NTwice.java
@@ -0,0 +1,9 @@
+package codingbat.string1;
+
+public class NTwice {
+    public String nTwice(String str, int n) {
+        return str.substring(0,n) +
+                str.substring(str.length()-n);
+    }
+
+}
