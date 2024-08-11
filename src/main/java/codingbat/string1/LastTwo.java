29707e6bf0fc1a8a6eaecdd3ccadb2c8d149d219
Zhanybek2 <zhanybekTalip2@gmail.com>
2024-08-11 12:45:28 +0600

    String1/lastTwo

---
+++ src/main/java/codingbat/string1/LastTwo.java
@@ -0,0 +1,15 @@
+package codingbat.string1;
+
+public class LastTwo {
+    public String lastTwo(String str) {
+        if (str.length() < 2){
+            return str;
+        }else {
+            return str.substring(0,str.length()-2) +
+                    str.charAt(str.length()-1) + str.charAt(str.length()-2);
+
+
+        }
+
+    }
+}
