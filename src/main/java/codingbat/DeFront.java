5980475224bef1dec25dbc650d5bb279af6fa6dc
Zhanybek2 <zhanybekTalip2@gmail.com>
2024-08-12 15:49:50 +0600

    String/feFront

---
+++ src/main/java/codingbat/DeFront.java
@@ -0,0 +1,24 @@
+package codingbat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeFront {
+    public String deFront(String str) {
+        int len = str.length();
+        String temp = "";
+
+        for (int i = 0; i < len; i++) {
+            if (i == 0 && str.charAt(i) == 'a')
+                temp += 'a';
+            else if (i == 1 && str.charAt(i) == 'b')
+                temp += 'b';
+            else if (i > 1)
+                temp += str.charAt(i);
+
+        }
+        return temp;
+
+
+    }
+}
