2450ab49694d8c9b0f101fabc0a51c33af6705d7
Zhanybek2 <zhanybekTalip2@gmail.com>
2024-08-12 17:19:02 +0600

    String1/withoutX

---
+++ src/main/java/WithoutX.java
@@ -0,0 +1,21 @@
+public class WithoutX {
+    public String withoutX(String str) {
+        if (str.length() == 0) {
+            return str;
+        }
+
+
+        if (str.charAt(0) == 'x') {
+            str = str.substring(1);
+        }
+
+
+        if (str.length() > 0 && str.charAt(str.length() - 1) == 'x') {
+            str = str.substring(0, str.length() - 1);
+        }
+
+        return str;
+    }
+
+}
+
